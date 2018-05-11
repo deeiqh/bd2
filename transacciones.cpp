36d424fd5648cc028664a2a04d600dd6c10543d5
k-st <kast46.15@gmail.com>
2018-05-11 15:48:10 -0500

    Create transacciones.cpp

---
+++ transacciones.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	return 0;
+}
+/*
+  t1  t2
+x [x1][x2] maneja las variables de las transacciones que van a ser escritas en la misma psicion del disco.
+
+x_disco;
+
+read(t1,x);
+restar(t1,x,n);
+read(t2,x);
+restar(t2,x,m);
+write(t1,x);
+read(t1,y);
+write(t2,x);
+sumar(t1,y,n);
+write(t1,y);
+
+el usuario ingresará los comandos que funcionarán tratando el x[][]
+
+*/
